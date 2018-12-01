929a36d2ca8c4938ed09dab86c25ff23094c7ca0
DIEGO IVAN PEREZ MICHEL <diego.ivan.perez.michel@gmail.com>
2018-11-30 22:53:03 -0800

    accouting support dev

---
+++ integracion.api/Services/AccountingService.cs
@@ -0,0 +1,17 @@
+﻿using integracion.api.Models;
+using ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace integracion.api.Services
+{
+    class AccountingService
+    {
+        public int RegisterSeat(MonthTransaction monthTransaction)
+        {
+            var client = new WSXContabilidadSoapClient();
+            return 0;
+        }
+    }
+}
