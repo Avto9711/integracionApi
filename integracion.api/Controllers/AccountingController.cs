44ea6f243938a2d2d1d53fde505a3fa9176e9360
DIEGO IVAN PEREZ MICHEL <diego.ivan.perez.michel@gmail.com>
2018-11-30 23:03:45 -0800

    accouting support dev-2

---
+++ integracion.api/Controllers/AccountingController.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.Linq;
+using integracion.api.Models;
+using integracion.api.Models.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ServiceReference1;
+using static ServiceReference1.WSXContabilidadSoapClient;
+
+namespace integracion.api.Controllers {
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountingController: ControllerBase {
+
+        private readonly IntegrationDbContext _integrationDbContext ;
+        public AccountingController(IntegrationDbContext integrationDbContext)
+        {
+         _integrationDbContext = integrationDbContext;   
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult<Transaction> Post([FromBody] MonthTransaction transaction)
+        {
+            var account = 71;
+            var debitAccount = 48;
+            var creditAccont = 48;
+            var description = "Nomina fecha: " + transaction.Date.ToString();
+
+            var client = new WSXContabilidadSoapClient(EndpointConfiguration.WSXContabilidadSoap);
+            var request = client.registrarAsientoAsync(description, account, (double)transaction.Amount, debitAccount, creditAccont);
+            var result = request.Result;
+
+            var generalLedger = new GeneralLedger{
+                Account = account.ToString(),
+                Date = transaction.Date,
+                Description = description,
+                Amount = transaction.Amount,
+                ExternalId = result.Body.registrarAsientoResult.ToString(),
+                MovementType = Models.Enums.MovementType.Db,
+                Status = Models.Enums.TrasactionStatus.Approved
+            };
+
+            _integrationDbContext.Set<GeneralLedger>().Add(generalLedger);
+            _integrationDbContext.SaveChanges();
+
+            return Created("", generalLedger);
+        }
+
+    }
+    
+}
--- integracion.api/Models/MonthTransaction.cs
+++ integracion.api/Models/MonthTransaction.cs
@@ -4,11 +4,10 @@ using System.Text;
 
 namespace integracion.api.Models
 {
-    class MonthTransaction
+    public class MonthTransaction
     {
         public int Month { get; set; }
-        public decimal Deduction { get; set; }
-        public decimal Entry { get; set; }
+        public decimal Amount { get; set; }
         public DateTime Date { get; set; }
     }
 }
